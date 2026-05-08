2154aee111b226da945d8a72b57645dc5ab13145
syllajunior1980 <sylla026@gmail.com>
2026-05-08 22:42:24 +0000

    Fix App.js UTF-8

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+﻿import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 const API = 'https://webscool.onrender.com/api';
